0904969f06ab65488600e551785746de02a7b008
mayor-b <betro1@gmail.com>
2020-01-08 21:09:28 -0800

    Then I added a new App thing.

---
+++ src/App/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>Oh heyyyy I made an app!</div>
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<div>This is my app</div>, document.getElementById('root'));
+import App from './App/App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('root'));
