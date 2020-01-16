4fa96f6412771acc5507ce5f741d83c076129e5a
mayor-b <betro1@gmail.com>
2020-01-15 21:15:51 -0800

    end of class monday 1/15

--- src/App/Nav/Nav.jsx
+++ src/App/Nav/Nav.jsx
@@ -3,7 +3,14 @@ import './Nav.scss';
 
 const Nav = () => {
     return (
-        <nav className={'Nav'}>My Magic Navigation</nav>
+        <nav className='Nav'>
+            <div className="hamburger">Hamburger</div>
+            <div className="links">
+                <a href="#">Welcome</a>
+                <a href="#">Services</a>
+                <a href="#">Contact</a>
+            </div>
+        </nav>
     );
 }
 
